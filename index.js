bc6b43e3a1749521ab34920434862b1172f08045
Nikolay Komarov <nik-komarov@yandex-team.ru>
2019-05-30 16:55:10 +0500

    Welcome.

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const { Alice, Reply } = require('yandex-dialogs-sdk');
 
 const alice = new Alice();
 
-alice.any(async ctx => Reply.text(`Эни финг`));
+const hasOpenedReceipt = userId => {
+    // TODO: check in DB.
 
-const server = alice.listen(process.env.PORT || 3000, '');
+    return false;
+};
+
+alice.any(ctx => {
+    return Reply
+        .text(`Привет! Я помогу тебе вести список заказанного в баре. ${hasOpenedReceipt(ctx.userId) ? '' : 'Для начала работы скажите - Открыть чек'}`)
+});
+
+alice.command(ctx => {
+    const words = ctx.nlu.tokens;
+
+    return words.includes('открыть') && words.includes('чек');
+}, ctx => {
+    return Reply
+        .text(hasOpenedReceipt(ctx.userId) ? 'Открываю' : 'У вас нет открытых чеков');
+});
+
+alice.command(/.+/, ctx => Reply.text('Не поняла'));
+
+alice.listen(process.env.PORT || 3000, '');

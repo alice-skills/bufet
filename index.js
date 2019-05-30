0cfe1e3891fe4379c60e2d2840f461925dabd86b
Aleksey Shevchenko <avshev@yandex-team.ru>
2019-05-30 18:35:39 +0300

    Удаление позиции в чеке

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Alice, Reply } = require('yandex-dialogs-sdk');
-const { findOne } = require('./lib/mongo');
+const { findOne, updateOne } = require('./lib/mongo');
 const { countCheckTotal } = require('./lib/utils');
 
 const alice = new Alice();
@@ -31,6 +31,26 @@ alice.command(ctx => ctx.nlu.tokens.includes('итог'), async ctx => {
     const doc = await findOne('itog', {uid: ctx.userId});
     return Reply
         .text(doc ? `Ваш счёт ${countCheckTotal(doc)} рублей` : 'У вас нет открытых чеков');
+});
+
+alice.command(ctx => {
+    const words = ctx.nlu.tokens;
+
+    return words.includes('удали');
+}, async ctx => {
+
+    const items = ctx.bill && ctx.bill.items || [] ;
+
+    if (items.length === 0) {
+        return Reply.text('В счете пусто, нечего удалять!')
+    }
+
+    const lastItem = items.pop();
+
+    await updateOne('checks', {_id: ctx.bill._id}, { $pop:  { items: 1 } });
+
+    return Reply
+        .text( `Удалила ${lastItem.title} стоимостью ${lastItem.cost} рублей`);
 });
 
 alice.command(/.+/, ctx => Reply.text('Не поняла'));

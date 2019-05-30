9ce53fafcfcb1f6389b6e8bb3b61b29c8b40a706
unikoid <unikoid@gmail.com>
2019-05-30 18:06:23 +0300

    Функция "показать счет" (#6)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Alice, Reply } = require('yandex-dialogs-sdk');
 const { findOne, insert } = require('./lib/mongo');
 const { countCheckTotal, hasOpenedReceipt } = require('./lib/utils');
+const { formatBill } = require('./lib/formatter');
 
 const alice = new Alice();
+
+const NO_RECEIPTS = 'У вас нет открытых чеков';
 
 alice.use(require('./lib/usermw'));
 
@@ -11,11 +14,33 @@ alice.any(ctx => {
         .text(`Привет! Я помогу тебе вести список заказанного в баре. ${hasOpenedReceipt(ctx) ? '' : 'Для начала работы скажите - Открыть чек'}`)
 });
 
+alice.command(ctx => {
+    const words = ctx.nlu.tokens;
+
+    return words.includes('открыть') && words.includes('чек');
+}, ctx => {
+    return Reply
+        .text(hasOpenedReceipt(ctx) ? 'Открываю' : NO_RECEIPTS);
+});
+
 // команду на подсчёт итога надо ещё доработать
 alice.command(ctx => ctx.nlu.tokens.includes('итог'), async ctx => {
     const doc = await findOne('itog', { uid: ctx.userId });
     return Reply
-        .text(doc ? `Ваш счёт ${countCheckTotal(doc)} рублей` : 'У вас нет открытых чеков');
+        .text(doc ? `Ваш счёт ${countCheckTotal(doc)} рублей` : NO_RECEIPTS);
+});
+
+alice.command(/покажи сч[её]т/i, ctx => {
+    const bill = ctx.bill;
+    let reply;
+    if (!bill) {
+        reply = NO_RECEIPTS;
+    } else if (!bill.items || !bill.items.length) {
+        reply = NO_RECEIPTS
+    } else {
+        reply = 'Ваш счёт: \n' + formatBill(bill);
+    }
+    return Reply.text(reply);
 });
 
 alice.command(['открыть чек', 'открой чек'], async ctx => {
